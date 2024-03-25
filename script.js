f10fab4a92a1ed76aff20282c05a44e98540fa52
Paweł Gościak <pawgosciak@gmail.com>
2024-03-25 18:44:43 +0100

    commit3

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-
-  var apiRoot = 'http://localhost:8080/v1/tasks';
-  var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-  var tasksContainer = $('[data-tasks-container]');
+$(document).ready(function () {
+  var apiRoot =
+    "https://3224fe13-9275-43c4-9a65-2709b96712b6-00-3ff4obgyzdtz9.picard.replit.dev/v1/tasks";
+  var datatableRowTemplate = $("[data-datatable-row-template]").children()[0];
+  var tasksContainer = $("[data-tasks-container]");
 
   // init
   getAllTasks();
@@ -10,19 +10,27 @@ $(document).ready(function() {
   function createElement(data) {
     var element = $(datatableRowTemplate).clone();
 
-    element.attr('data-task-id', data.id);
-    element.find('[data-task-name-section] [data-task-name-paragraph]').text(data.title);
-    element.find('[data-task-name-section] [data-task-name-input]').val(data.title);
+    element.attr("data-task-id", data.id);
+    element
+      .find("[data-task-name-section] [data-task-name-paragraph]")
+      .text(data.title);
+    element
+      .find("[data-task-name-section] [data-task-name-input]")
+      .val(data.title);
 
-    element.find('[data-task-content-section] [data-task-content-paragraph]').text(data.content);
-    element.find('[data-task-content-section] [data-task-content-input]').val(data.content);
+    element
+      .find("[data-task-content-section] [data-task-content-paragraph]")
+      .text(data.content);
+    element
+      .find("[data-task-content-section] [data-task-content-input]")
+      .val(data.content);
 
     return element;
   }
 
   function handleDatatableRender(data) {
     tasksContainer.empty();
-    data.forEach(function(task) {
+    data.forEach(function (task) {
       createElement(task).appendTo(tasksContainer);
     });
   }
@@ -32,16 +40,16 @@ $(document).ready(function() {
 
     $.ajax({
       url: requestUrl,
-      method: 'GET',
-        success: handleDatatableRender
-     });
+      method: "GET",
+      success: handleDatatableRender,
+    });
   }
 
   function handleTaskUpdateRequest() {
     var parentEl = $(this).parent().parent();
-    var taskId = parentEl.attr('data-task-id');
-    var taskTitle = parentEl.find('[data-task-name-input]').val();
-    var taskContent = parentEl.find('[data-task-content-input]').val();
+    var taskId = parentEl.attr("data-task-id");
+    var taskTitle = parentEl.find("[data-task-name-input]").val();
+    var taskContent = parentEl.find("[data-task-content-input]").val();
     var requestUrl = apiRoot;
 
     $.ajax({
@@ -49,32 +57,36 @@ $(document).ready(function() {
       method: "PUT",
       processData: false,
       contentType: "application/json; charset=utf-8",
-      dataType: 'json',
+      dataType: "json",
       data: JSON.stringify({
         id: taskId,
         title: taskTitle,
-        content: taskContent
+        content: taskContent,
       }),
-      success: function(data) {
-        parentEl.attr('data-task-id', data.id).toggleClass('datatable__row--editing');
-        parentEl.find('[data-task-name-paragraph]').text(taskTitle);
-        parentEl.find('[data-task-content-paragraph]').text(taskContent);
-      }
+      success: function (data) {
+        parentEl
+          .attr("data-task-id", data.id)
+          .toggleClass("datatable__row--editing");
+        parentEl.find("[data-task-name-paragraph]").text(taskTitle);
+        parentEl.find("[data-task-content-paragraph]").text(taskContent);
+      },
     });
   }
 
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parent().parent();
-    var taskId = parentEl.attr('data-task-id');
+    var taskId = parentEl.attr("data-task-id");
     var requestUrl = apiRoot;
 
     $.ajax({
-      url: requestUrl + '/' + taskId,
-      method: 'DELETE',
-      success: function() {
-        parentEl.slideUp(400, function() { parentEl.remove(); });
-      }
-    })
+      url: requestUrl + "/" + taskId,
+      method: "DELETE",
+      success: function () {
+        parentEl.slideUp(400, function () {
+          parentEl.remove();
+        });
+      },
+    });
   }
 
   function handleTaskSubmitRequest(event) {
@@ -87,37 +99,49 @@ $(document).ready(function() {
 
     $.ajax({
       url: requestUrl,
-      method: 'POST',
+      method: "POST",
       processData: false,
       contentType: "application/json; charset=utf-8",
-      dataType: 'json',
+      dataType: "json",
       data: JSON.stringify({
         title: taskTitle,
-        content: taskContent
+        content: taskContent,
       }),
-      complete: function(data) {
-        if(data.status === 200) {
+      complete: function (data) {
+        if (data.status === 200) {
           getAllTasks();
         }
-     }
+      },
     });
   }
 
   function toggleEditingState() {
     var parentEl = $(this).parent().parent();
-    parentEl.toggleClass('datatable__row--editing');
+    parentEl.toggleClass("datatable__row--editing");
 
-    var taskTitle = parentEl.find('[data-task-name-paragraph]').text();
-    var taskContent = parentEl.find('[data-task-content-paragraph]').text();
+    var taskTitle = parentEl.find("[data-task-name-paragraph]").text();
+    var taskContent = parentEl.find("[data-task-content-paragraph]").text();
 
-    parentEl.find('[data-task-name-input]').val(taskTitle);
-    parentEl.find('[data-task-content-input]').val(taskContent);
+    parentEl.find("[data-task-name-input]").val(taskTitle);
+    parentEl.find("[data-task-content-input]").val(taskContent);
   }
 
-  $('[data-task-add-form]').on('submit', handleTaskSubmitRequest);
+  $("[data-task-add-form]").on("submit", handleTaskSubmitRequest);
 
-  tasksContainer.on('click','[data-task-edit-button]', toggleEditingState);
-  tasksContainer.on('click','[data-task-edit-abort-button]', toggleEditingState);
-  tasksContainer.on('click','[data-task-submit-update-button]', handleTaskUpdateRequest);
-  tasksContainer.on('click','[data-task-delete-button]', handleTaskDeleteRequest);
+  tasksContainer.on("click", "[data-task-edit-button]", toggleEditingState);
+  tasksContainer.on(
+    "click",
+    "[data-task-edit-abort-button]",
+    toggleEditingState
+  );
+  tasksContainer.on(
+    "click",
+    "[data-task-submit-update-button]",
+    handleTaskUpdateRequest
+  );
+  tasksContainer.on(
+    "click",
+    "[data-task-delete-button]",
+    handleTaskDeleteRequest
+  );
 });
